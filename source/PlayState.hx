9ec45033d8ffb80d190f4b318d33cba7c1f09319
Moneky <officialsunday@protonmail.com>
2025-10-24 13:50:43 +0100

    apple and snake hx files

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,18 @@
 package;
 
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.util.FlxColor;
+
 
 class PlayState extends FlxState
 {
+	var snakeSquare:FlxSprite;
 	override public function create()
 	{
 		super.create();
+		snakeSquare = new FlxSprite().makeGraphic(100, 100, FlxColor.GREEN);
+		add(snakeSquare);
 	}
 
 	override public function update(elapsed:Float)

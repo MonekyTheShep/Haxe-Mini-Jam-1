c129e1c5296fc39bac1b40d980d2065b962be44d
Moneky <officialsunday@protonmail.com>
2025-10-24 22:46:32 +0100

    snake main logic done. needs the tail part to be done.
    we have an array to store positions in format [0,0] [1,0] [2,0]
    [x,y]
    then
    with those positions
    we put them in a grid
    by multiplying
    each of those positions
    by *25
    by *20
    +5 for a margin
    THEN
    we draw the cube
    with those values
    we have a variable
    snakeParts
    which is a interger
    which tells us how long our snake is
    we loop through 0 to snakeParts
    the position is updated inside of update with move();
    but its way to fast
    it needs a delay

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,20 +1,38 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.util.FlxColor;
 class PlayState extends FlxState
 {
-	var snakeSquare:FlxSprite;
+	var snake = new Snake(0, 0, "RIGHT");
+
 	override public function create()
 	{
 		super.create();
-		var snake = new Snake();
 		add(snake);
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		if (FlxG.keys.pressed.A)
+		{
+			snake.direction = "LEFT";
+		}
+		else if (FlxG.keys.pressed.D)
+		{
+			snake.direction = "RIGHT";
+		}
+		else if (FlxG.keys.pressed.W)
+		{
+			snake.direction = "UP";
+		}
+		else if (FlxG.keys.pressed.S)
+		{
+			snake.direction = "DOWN";
+		}
+
 	}
 }
--- source/Snake.hx
+++ source/Snake.hx
@@ -2,22 +2,69 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.group.FlxGroup;
 import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
 
 class Snake extends FlxSprite
 {
-	public function new(x:Float = 0, y:Float = 0)
+	var snakeParts:Int = 6;
+	var position:Array<Float> = [0, 0];
+	var tileSize:Int = 20;
+	var snakeColor = FlxColor.GREEN;
+	var tailColor = FlxColor.RED;
+	public var direction:String = "";
+
+	public function new(x:Float = 0, y:Float = 0, direction:String = "RIGHT")
 	{
 		this.x = x;
 		this.y = y;
+		this.direction = direction;
 
 		super(this.x, this.y);
-
-		makeGraphic(20, 20, FlxColor.GREEN);
+		makeGraphic(tileSize, tileSize);
 	}
+
+	override public function draw():Void
+	{
+		super.draw();
+		var previousPos = [0, 0];
+		for (i in 0...snakeParts){
+			if (i == 0){
+				this.x = position[0] * tileSize + 5;
+				this.y = position[1] * tileSize + 5;
+				this.color = snakeColor;
+				super.draw();
+			} 
+			else 
+			{
+				this.x = i;
+				this.y = i;
+				this.color = tailColor;
+				super.draw();
+			}
+		}
+
+
+	}
+
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		trace(this.direction);
+		move();
+	}
+	function move() {
+		if (direction == "LEFT") {
+			position[0] -= 1;
+		} else if (direction == "RIGHT") {
+			position[0] += 1;
+			trace(position[0]);
+		} else if (direction == "UP") {
+			position[1] -= 1;
+		} else if (direction == "DOWN") {
+			position[1] += 1;
+		}
 	}
 }

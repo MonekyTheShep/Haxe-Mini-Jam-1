8e45ce1dd25e054e22215727ea6eddf86b296a39
Moneky <officialsunday@protonmail.com>
2025-10-25 18:04:13 +0100

    snake logic
    snake logic

--- source/Snake.hx
+++ source/Snake.hx
@@ -7,6 +7,7 @@ import flixel.util.FlxColor;
 import flixel.util.FlxDestroyUtil;
 import flixel.util.FlxSpriteUtil;
 import flixel.util.FlxTimer;
+import haxe.display.Display.SignatureItemKind;
 
 enum SnakeDirection
 {
@@ -16,51 +17,42 @@ enum SnakeDirection
 	DOWN;
 }
 
-class Snake extends FlxSprite
+class Snake extends FlxGroup
 {
-	var snakeParts:Int = 6;
-	var position:Array<Float> = [0, 0];
 	var snakeColor = FlxColor.GREEN;
 	var tailColor = FlxColor.RED;
+
+	var snakeHead:FlxSprite = new FlxSprite();
+
+	var snakeBody:FlxGroup = new FlxGroup();
+
+	var snakeLength:Int = 5;
+
+	var prevPositions:Array<Array<Float>> = [];
 
 	public var direction:Null<SnakeDirection> = null;
 
 	public function new(x:Float = 0, y:Float = 0, ?direction:SnakeDirection = RIGHT)
 	{
-		this.setPosition(x, y);
+		super();
+
 		this.direction = direction;
-
-		super(x, y);
-		makeGraphic(Constants.TILE_SIZE, Constants.TILE_SIZE);
+		snakeHead.makeGraphic(Constants.TILE_SIZE, Constants.TILE_SIZE);
+		snakeHead.setPosition(x, y);
+		snakeHead.color = snakeColor;
+		add(snakeHead);
+		add(snakeBody);
 		doTimer();
+		for (i in 0...snakeLength)
+		{
+			var tailSquare:FlxSprite = new FlxSprite();
+			tailSquare.makeGraphic(Constants.TILE_SIZE, Constants.TILE_SIZE);
+			tailSquare.color = tailColor;
+			snakeBody.add(tailSquare);
+		}
+
 	}
 
-	var previousPositions:Array<Array<Float>> = [];
-	var prevPos = [];
-	@:noCompletion override public function draw():Void
-	{
-		super.draw();
-		// var previousPos = [0, 0];
-		// for (i in 0...snakeParts)
-		// {
-		// 	if (i == 0)
-		// 	{
-		// 		this.x = position[0] * Constants.TILE_SIZE + 5;
-		// 		this.y = position[1] * Constants.TILE_SIZE + 5;
-		// 		prevPos = [this.x, this.y];
-		// 		trace(prevPos);
-		// 		this.color = snakeColor;
-		// 		super.draw();
-		// 	}
-		// 	else
-		// 	{
-		// 		this.x = previousPos[0] - prevPos[0];
-		// 		this.y = previousPos[1] + prevPos[1];
-		// 		this.color = tailColor;
-		// 		super.draw();
-		// 	}
-		// }
-	}
 
 	public var movementInterval:Float = 8;
 
@@ -73,33 +65,61 @@ class Snake extends FlxSprite
 		}
 		new FlxTimer().start(movementInterval / FlxG.updateFramerate, doTimer);
 		move();
+
 	}
 
 	@:noCompletion override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
 		trace(this.direction);
-		// move();
+		// loop through snakebody members
+		for (member in snakeBody.members)
+		{
+			// create a variable for each member
+			var tails:FlxSprite = cast member;
+			// wait for the previous positions to equal the snake body length
+			// it has -1 because it also stores the head
+			if (prevPositions.length - 1 == snakeBody.length)
+			{
+				// use the previous position from the array
+				tails.x = prevPositions[snakeBody.members.indexOf(member)][0];
+				tails.y = prevPositions[snakeBody.members.indexOf(member)][1];
+			}
+		}
+
 	}
 
 	function move():Void
 	{
+		// every 200 ms this move function is called, so use it to store the previous values + the head
+		prevPositions = [];
+		prevPositions.push([snakeHead.x, snakeHead.y]);
+
+		// store the tail body positions.
+		for (member in snakeBody.members)
+		{
+			var tails:FlxSprite = cast member;
+			prevPositions.push([tails.x, tails.y]);
+		}
+		
 		if (direction != null)
 		{
 			switch (direction)
 			{
 				case LEFT:
-					this.x -= Constants.TILE_SIZE;
+					snakeHead.x -= Constants.TILE_SIZE;
 				case RIGHT:
-					this.x += Constants.TILE_SIZE;
-					trace(position[0]);
+					snakeHead.x += Constants.TILE_SIZE;
 				case UP:
-					this.y -= Constants.TILE_SIZE;
+					snakeHead.y -= Constants.TILE_SIZE;
 				case DOWN:
-					this.y += Constants.TILE_SIZE;
+					snakeHead.y += Constants.TILE_SIZE;
 			}
-			FlxSpriteUtil.screenWrap(this);
+			FlxSpriteUtil.screenWrap(snakeHead);
 		}
-		trace(previousPositions);
+		// debug shit
+		trace(prevPositions);
+		trace(prevPositions.length);
+	
 	}
 }

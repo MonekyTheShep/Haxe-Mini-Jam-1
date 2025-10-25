92ec5973e558b8e56f19bfdf92000c2fe1744f45
HackDev <80620538+Hackx2@users.noreply.github.com>
2025-10-25 13:15:50 +0100

    you can eat now...

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,76 +1,101 @@
 package;
 
 import flixel.FlxG;
-import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.group.FlxSpriteGroup;
+import flixel.util.FlxCollision;
 import flixel.util.FlxColor;
-import haxe.Timer;
-import openfl.display.Sprite;
 import openfl.filters.ShaderFilter;
 
 class PlayState extends FlxState
 {
+	/**
+	 * The Snake Player Thingy...
+	 */
 	var snake:Snake;
 
-	var apple_timer:Timer;
+	#if SHADERS_ALLOWED
+	/**
+	 * The CRT shader.
+	 * This shader is used for the whole game.
+	 */
 	var crt:CrtShader;
+	#end
+
+	/**
+	 * The Apple collectable group.
+	 */
+	var appleGroup:FlxTypedSpriteGroup<Apple>;
 
 	@:dox(hide) override public function create()
 	{
 		super.create();
-		FlxG.camera.pixelPerfectRender = true;
-		FlxG.camera.pixelPerfectShake = true;
+		FlxG.camera.pixelPerfectRender = FlxG.camera.pixelPerfectShake = true;
 
 		add(new GridSprite(FlxColor.WHITE));
+		add(appleGroup = new FlxTypedSpriteGroup<Apple>());
 		add(snake = new Snake(0, 0));
-		apple_timer = new Timer(2000);
-		apple_timer.run = spawnApple;
 
-		crt = new CrtShader();
-		FlxG.camera.filters = [new ShaderFilter(crt)];
+		#if SHADERS_ALLOWED
+		// Set the shader
+		FlxG.camera.filters = [new ShaderFilter(crt = new CrtShader())];
+		#end
 	}
 
-	public function spawnApple():Void
-	{
-		var apple:Apple = new Apple();
-		add(apple);
-	}
+	/**
+	 * Whether you can spawn apples or not...
+	 */
+	var CAN_SPAWN_APPLES:Bool = true;
 
-	@:dox(hide) override function destroy():Void
-	{
-		if (apple_timer != null)
-			apple_timer.stop();
-		super.destroy();
-	}
+	@:noCompletion var _prevElapsed:Float = 0;
+	@:noCompletion var _appleSpawnTimer:Float = 0;
 
-	var prevElapsed:Float = 0;
-	@:dox(hide) override public function update(elapsed:Float)
+	@:dox(hide) override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
-		if (crt != null && crt.iTime != null && crt.iTime.value != null)
+		// Spawn the apples, every 2 seconds.
+		if (CAN_SPAWN_APPLES)
 		{
-			crt.iTime.value[0] = prevElapsed + elapsed;
-			prevElapsed = crt.iTime.value[0];
+			_appleSpawnTimer += elapsed;
+
+			if (_appleSpawnTimer >= 2)
+			{
+				appleGroup.add(new Apple());
+				_appleSpawnTimer = 0;
+			}
 		}
 
+		#if SHADERS_ALLOWED
+		// Update The CRT Shader...
+		if (crt != null && crt.iTime != null && crt.iTime.value != null)
+		{
+			crt.iTime.value[0] = _prevElapsed + elapsed;
+			_prevElapsed = crt.iTime.value[0];
+		}
+		#end
+
+		// Handle Apple Collisions...
+		appleGroup.forEachAlive((spr:Apple) ->
+		{
+			if (spr != null && FlxCollision.pixelPerfectCheck(snake, spr))
+			{
+				appleGroup.remove(spr);
+				spr.kill();
+				spr.destroy();
+			}
+		});
+
+		// Handle Movement...
 		if (snake != null)
 		{
-			if (FlxG.keys.pressed.A)
-			{
+			if (FlxG.keys.anyJustPressed([A, LEFT]))
 				snake.direction = LEFT;
-			}
-			else if (FlxG.keys.pressed.D)
-			{
+			else if (FlxG.keys.anyJustPressed([D, RIGHT]))
 				snake.direction = RIGHT;
-			}
-			else if (FlxG.keys.pressed.W)
-			{
+			else if (FlxG.keys.anyJustPressed([W, UP]))
 				snake.direction = UP;
-			}
-			else if (FlxG.keys.pressed.S)
-			{
+			else if (FlxG.keys.anyJustPressed([S, DOWN]))
 				snake.direction = DOWN;
-			}
 		}
 	}
 }

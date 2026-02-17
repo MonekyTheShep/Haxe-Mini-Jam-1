7bcbe6987e1e0c4a9c8239f25763823ae732afc4
Moneky <237399958+MonekyTheShep@users.noreply.github.com>
2026-02-17 01:27:40 +0000

    Fix apple spawning logic

--- source/utility/AppleHandling.hx
+++ source/utility/AppleHandling.hx
@@ -48,11 +48,11 @@ class AppleHandling
 	// spawn apple at random location
 	public function randomPosition():FlxPoint
 	{
-		final padding:Int = Constants.TILE_SIZE * 2;
-		var x:Float = (FlxG.random.int(Std.int(padding / Constants.TILE_SIZE),
-			Std.int((FlxG.width - padding) / Constants.TILE_SIZE) - 1)) * Constants.TILE_SIZE;
-		var y:Float = (FlxG.random.int(Std.int(padding / Constants.TILE_SIZE),
-			Std.int((FlxG.height - padding) / Constants.TILE_SIZE) - 1)) * Constants.TILE_SIZE;
-		return FlxPoint.get(x, y);
+		var x:Float =  (FlxG.random.int(0,
+			Std.int(FlxG.width / Constants.TILE_SIZE)));
+		var y:Float = (FlxG.random.int(0,
+			Std.int(FlxG.height / Constants.TILE_SIZE)));
+
+		return FlxPoint.weak(x * Constants.TILE_SIZE, y * Constants.TILE_SIZE);
 	}
 }

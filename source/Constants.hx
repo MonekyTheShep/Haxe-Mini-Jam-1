e53cdbe1625516f34320c93da3e8f4cc695f7ad4
Moneky <237399958+MonekyTheShep@users.noreply.github.com>
2026-03-25 07:34:45 +0000

    refactor (snake) timer

--- source/Constants.hx
+++ source/Constants.hx
@@ -2,5 +2,5 @@ package;
 
 final class Constants {
     public static inline final TILE_SIZE:Int = 20;
-	public static inline final movementInterval:Float = 8;
+	public static inline final SNAKE_MOVE_INTERVAL:Float = 0.125;
 }
